bb42cbd6e36e2204a9286379c323d3b59903d340
Kathe-Adiaz <kathegd866@gmail.com>
2025-04-06 18:18:19 -0500

    Update libro_investigacion.py

--- Libro/libro_investigacion.py
+++ Libro/libro_investigacion.py
@@ -4,10 +4,10 @@ from Libro.libro import Libro
 class LibroInvestigacion(Libro): 
     def __init__(self, titulo: str, autor: str, precio: float, facultad: str):
         super().__init__(titulo, autor, precio)
-        self.facultad = facultad  # Atributo específico
+        self.facultad = facultad  
 
-    def calcular_descuento(self) -> float:  # Polimorfismo
-        return self.precio * 0.25  # 25% descuento
+    def calcular_descuento(self) -> float:  
+        return self.precio * 0.25  
     
     def __str__(self):
         return super().__str__() + f"\nFacultad: {self.facultad}\nTipo: Libro de Investigación UdeC (25% descuento)"
